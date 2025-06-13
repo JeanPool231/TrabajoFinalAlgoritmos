7918ad131fe619f94d0c34cb7666ac3f6eb57848
GZ-99 <gabrielvilchezvite@gmail.com>
2025-06-13 17:40:38 -0500

    Add files via upload

---
+++ Final/Administrador.h
@@ -0,0 +1,29 @@
+#pragma once //Esto es mio
+#include "ListaEnlazada.h"
+#include "ListaCircularSimple.h"
+#include "AVLTree.h"
+#include "Profesor.h"
+#include <conio.h>
+using namespace std;
+
+class Administrador
+{
+public:
+	Administrador();
+	~Administrador();
+
+	void ver_cursos(ListaEnlazada<Curso*>& curso);
+	void añadir_cursos(ListaEnlazada<Curso*>& curso);
+	void eliminar_cursos(ListaEnlazada<Curso*>& curso);
+	void ver_estudiantes();
+	void añadir_estudiantes();
+	void eliminar_estudiantes();
+	void ver_profesores(AVLTree<Profesor*>& profe);
+	void añadir_profesores(AVLTree<Profesor*>& profe);
+	void eliminar_profesores(AVLTree<Profesor*>& profe);
+	void ver_instituciones();
+	void añadir_instituciones();
+	void eliminar_instituciones();
+
+	void menu_admin(ListaEnlazada<Curso*>& curso, AVLTree<Profesor*>& profe);
+};
