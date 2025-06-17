c8af3521e6c80215365df5f912ea340c0f0565b4
GZ-99 <gabrielvilchezvite@gmail.com>
2025-06-17 18:29:41 -0500

    Update Sistema.h

--- Final/Sistema.h
+++ Final/Sistema.h
@@ -2,20 +2,38 @@
 #include <iostream>
 #include "Profesor.h"
 #include "Curso.h"
+#include "Institucion.h"
 #include "HashUtil.h"
+#include "ListaEnlazada.h"
+#include "Usuario.h"
+#include "Estudiante.h"
+#include "Profesor.h"
+#include "Institucion.h"
+#include "Administrador.h"
+
 using namespace std;
 
 class Sistema {
 private:
-
+	Estudiante* estudiante = new Estudiante();
+	Profesor* profesor = new Profesor();
+	Institucion* institucion = new Institucion();
+	ListaEnlazada<Curso> cursos;
+	ListaEnlazada<Usuario> usuarios;
+	void menuInstitucion();
 	void menuPrincipal();
 	void registroEstudiante();
+	void registroProfesor();
+	void registroInstitucion();
 	void registrarse();
 	void iniciarSesion();
+	void menuAdmin();
 	bool validarCorreo();
 	void inicializarDatos();
 	void menuProfesor();
+	void menuEstudiante();
+	void cursosEstudiante();
+	void perfilEstudiante();
 public:
 	void iniciarPrograma();
 };
-
