8c0f6936fc76eaf769f9b12e68a127d865e6ddac
GZ-99 <gabrielvilchezvite@gmail.com>
2025-07-01 18:55:30 -0500

    Update Administrador.h

--- Final/Administrador.h
+++ Final/Administrador.h
@@ -15,18 +15,18 @@ public:
 	Administrador();
 	~Administrador();
 
-	void guardar(Usuario usuario);
-	void ver_cursos(ListaEnlazada<Curso*>& curso);
-	void añadir_cursos(ListaEnlazada<Curso*>& curso);
-	void eliminar_cursos(ListaEnlazada<Curso*>& curso);
-	void ver_estudiantes(ListaEnlazada<Estudiante*>& estu);
-	void añadir_estudiantes(ListaEnlazada<Estudiante*>& estu);
+	void guardar(Usuario usuario); //listo
+	void ver_cursos(ListaEnlazada<Curso*>& curso); //listo
+	void añadir_cursos(ListaEnlazada<Curso*>& curso); //listo
+	void eliminar_cursos(ListaEnlazada<Curso*>& curso); //listo
+	void ver_estudiantes(ListaEnlazada<Estudiante*>& estu); //listo
+	void añadir_estudiantes(ListaEnlazada<Estudiante*>& estu); //listo
 	void eliminar_estudiantes(ListaEnlazada<Estudiante*>& estu);
-	void ver_profesores(AVLTree<Profesor*>& profe);
-	void añadir_profesores(AVLTree<Profesor*>& profe);
-	void eliminar_profesores(AVLTree<Profesor*>& profe);
-	void ver_instituciones(AVLTree<Institucion*>& inst);
-	void añadir_instituciones(AVLTree<Institucion*>& inst);
+	void ver_profesores(AVLTree<Profesor*>& profe); //listo
+	void añadir_profesores(AVLTree<Profesor*>& profe); //listo
+	void eliminar_profesores(AVLTree<Profesor*>& profe); //listo
+	void ver_instituciones(AVLTree<Institucion*>& inst); //listo
+	void añadir_instituciones(AVLTree<Institucion*>& inst); //listo
 	void eliminar_instituciones(AVLTree<Institucion*>& inst);
 
 	void menu_admin(ListaEnlazada<Curso*>& curso, ListaEnlazada<Estudiante*>& estu, AVLTree<Profesor*>& profe, AVLTree<Institucion*>& inst);
